724f7ac0743d11ba00ccb0d15423c3833c9579c0
harikiran09 <hari.chebrol@gmail.com>
2026-08-08 17:01:30 +0530

    after adding thread examples

---
+++ src/sample2/Task.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package sample2;
+
+import java.util.concurrent.atomic.LongAccumulator;
+
+/**
+ * @author bc887d
+ *
+ */
+public class Task implements Runnable {
+
+	private LongAccumulator counter = null;
+
+	public Task(LongAccumulator counter) {
+		this.counter = counter;
+	}
+
+	@Override
+	public void run() {
+		counter.accumulate(1);
+
+	}
+
+}
