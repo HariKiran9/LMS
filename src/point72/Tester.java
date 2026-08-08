bacf39ff16670eeb48373168f8374ba817f82010
harikiran09 <hari.chebrol@gmail.com>
2026-08-08 16:59:04 +0530

    after adding Arcesium coding test files

--- src/point72/Tester.java
+++ src/point72/Tester.java
@@ -23,7 +23,7 @@ class AirJet extends AirPlane {
 
 public class Tester {
 
-	public static void main(String args[]) {
+	public static void main(String[] args) {
 		try {
 			new AirJet();
 		} catch (IOException e) {
